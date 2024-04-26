17d6994ab5a60370758273179f7af1df3ef91f8e
Satyarth Ojha <satyarth@Satyarths-MacBook-Air.local>
2024-04-26 12:30:24 +0530

    corrected log in logic get_token.go

--- logic/get_token.go
+++ logic/get_token.go
@@ -2,6 +2,7 @@ package logic
 
 import (
 	"context"
+	"fmt"
 	"log/slog"
 	"net/http"
 
@@ -12,7 +13,7 @@ import (
 func GetToken(ctx context.Context, userID string) (*dto.LoginResp, *dto.CommonError) {
 	accessToken, refreshToken, expiresIn, err := auth.GetToken(userID)
 	if err != nil {
-		slog.WarnContext(ctx, "failed to regenerate token for user: %s", userID)
+		slog.WarnContext(ctx, fmt.Sprintf("failed to regenerate token for user: %s", userID))
 		return nil, &dto.CommonError{Err: err, StatusCode: http.StatusInternalServerError}
 	}
 	return &dto.LoginResp{
